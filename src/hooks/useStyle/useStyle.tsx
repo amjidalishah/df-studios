77279d0ad1c7ca4a4797f6cf8e4cdbd9b4ef1fac
Lmraza98 <lmraza98@gmail.com>
2023-01-31 03:34:04 -0500

    cool

---
+++ src/hooks/useStyle/useStyle.tsx
@@ -0,0 +1,35 @@
+import { useState, createContext, useContext, ReactNode } from 'react'
+
+type authContextType = {
+    style: object | null;
+};
+
+const authContextDefaultValues: authContextType = {
+    style: null
+};
+
+const AuthContext = createContext<authContextType>(authContextDefaultValues);
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
+
+type Props = {
+    children: ReactNode;
+};
+
+export function AuthProvider({ children }: Props) {
+    const [ style, setStyle ] = useState<authContextType['style']>(null);
+
+    const value = {
+        style
+    };
+
+    return (
+    <>
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    </>
+);
+}
