40c35e6254398fe2d11ec935b8c7bac147c9d1d5
Lmraza98 <lmraza98@gmail.com>
2023-03-18 18:06:49 -0400

    fixed CheckoutForm

--- components/index.tsx
+++ components/index.tsx
@@ -1,12 +1,8 @@
 "use client"
 import Logo from './Logo'
 import Button from './Button'
-import Room from './Room'
 import Profile from './Profile'
-import Hero from './Hero'
-import Footer from './Footer'
 import Nav from './Nav'
-import RoomCard from './RoomCard'
 import CheckoutForm from './CheckoutForm'
 
-export { Logo, Button, Room, Profile, Footer, Hero, Nav, RoomCard, CheckoutForm }
+export { Logo, Button, Profile, Nav, CheckoutForm }
