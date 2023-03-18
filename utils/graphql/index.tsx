c3d435137e9962362611afdd5303fc089cfbfce6
Lmraza98 <lmraza98@gmail.com>
2023-03-18 18:22:58 -0400

    fixed CheckoutForm

--- utils/graphql/index.tsx
+++ utils/graphql/index.tsx
@@ -1,4 +1,4 @@
-import { GET_EVENTS } from './events'
-import { GET_ROOMS, GET_HOME } from './pages'
+import { GET_EVENTS } from './queries/events'
+import { GET_ROOMS, GET_HOME } from './queries/pages'
 
 export { GET_EVENTS, GET_ROOMS, GET_HOME }
